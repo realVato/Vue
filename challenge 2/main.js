8210fb38aae4959691a09c1e2323a494d028ca78
realVato <44781061+realVato@users.noreply.github.com>
2019-12-05 08:31:58 +0200

    chall 2

---
+++ challenge 2/main.js
@@ -0,0 +1,9 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        description: 'Your feet will thank you!',
+        image: './vmSocks-green-onWhite.jpg',
+        portofolio: 'https://tomastalalas.netlify.com/',
+    }
+})
