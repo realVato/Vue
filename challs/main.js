f14b63b0878eb65e4d9718afdd2ec9410d26a66c
realVato <44781061+realVato@users.noreply.github.com>
2019-12-06 04:24:17 +0200

    @mouseover

--- challs/main.js
+++ challs/main.js
@@ -6,6 +6,18 @@ const app = new Vue({
         image: './vmSocks-green-onWhite.jpg',
         portofolio: 'https://tomastalalas.netlify.com/',
         onSale: true,
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green',
+                variantImage: './vmSocks-green-onWhite.jpg'
+            },
+            {
+                    variantId: 2235,
+                    variantColor: 'blue',
+                    variantImage: './vmSocks-blue-onWhite.jpg'
+            }
+        ],
         sizes: ['Extra small', 'Small', 'Medium', 'Large', 'Extra large'],
         cart: 0,
     },
@@ -15,6 +27,9 @@ const app = new Vue({
         },
         removeFromCart() {
             this.cart -= 1;
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage;
         }
     }
 })
