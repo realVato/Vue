19bd61d622f54f8d3dd8ed650498ef6e9c409110
realVato <44781061+realVato@users.noreply.github.com>
2019-12-06 03:30:08 +0200

    chall 3

---
+++ challenge 3/main.js
@@ -0,0 +1,9 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        description: 'Your feet will thank you!',
+        image: './vmSocks-green-onWhite.jpg',
+        onSale: true,
+    }
+})
